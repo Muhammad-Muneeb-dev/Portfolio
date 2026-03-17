677b1d7513a630351f5f6985a588816a158b63a8
Muhammad Muneeb <m.muneeb.dev@gmail.com>
2026-03-17 17:17:38 +0500

    hero added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
+import Hero from "./sections/hero";
 
 
 const App = () => {
   return (
     <div className="relative w-screen min-h-screen overflow-x-auto">
       <Navbar/>
+      <Hero/>
 
-      <section id="home" className="min-h-screen"/>
-      <section id="services" className="min-h-screen"/>
+      {/* <section id="home" className="min-h-screen"/>
+      <section id="services" className="min-h-screen"/> */}
 
     </div>
   )
